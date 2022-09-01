66e9571fe0f5a02cfe0eb7a65a56f620dcbd0ef2
egg <rtg@ff.com>
2022-09-01 14:40:44 +0530

    pre-deploy TS fixes 6

--- pages/components/ListComponent.tsx
+++ pages/components/ListComponent.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export function UnorderedListComponent(props: any) {
+export const UnorderedListComponent = (props: any) => {
   return <ul className="mt-2 list-disc pl-8" {...props} />;
-}
-export function ListComponent(props: any) {
+};
+export const ListComponent = (props: any) => {
   return <li className="text-lg font-normal text-gray-700 " {...props} />;
-}
+};
