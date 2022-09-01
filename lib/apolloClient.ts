d493578c46b0068f9acbc27b0663964fa5ce343d
egg <rtg@ff.com>
2022-09-01 14:18:45 +0530

    added non-null assertion to client

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from "graphql-request";
 
-const client = new GraphQLClient(process.env.CMS_URL);
+const client = new GraphQLClient(process.env.CMS_URL!);
 
 export default client;
